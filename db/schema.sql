8223b10f41eaeb2b4a47328df9fae36287c7086d
Val Nimirenco <valnimirenco@gmail.com>
2021-02-03 20:49:47 -0800

    added schema

---
+++ db/schema.sql
@@ -0,0 +1,10 @@
+DROP DATABASE IF EXISTS burgers_DB;
+CREATE DATABASE burgers_DB;
+
+USE burgers_DB;
+
+CREATE TABLE burgers (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+burger_name VARCHAR (50) NOT NULL.
+devoured BOOLEAN defaul 0
+);
