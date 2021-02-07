a1557c83cb14c3bcb6d45e909f5d52df2326e1b8
Val Nimirenco <valnimirenco@gmail.com>
2021-02-07 15:41:45 -0800

    added more seed data exit

--- db/seed.sql
+++ db/seed.sql
@@ -1,1 +1,4 @@
-INSERT INTO burgers (burger_name) VALUES ("chicken"), ("cheese"), ("tomatos");
+INSERT INTO burgers (burger_name) VALUES ("Chicken");
+INSERT INTO burgers (burger_name) VALUES ("Double Double");
+INSERT INTO burgers (burger_name) VALUES ("BigMac");
+INSERT INTO burgers (burger_name) VALUES ("Texas burger");
