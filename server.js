2b2066741f52fcbf4786ebf5b999e562548008d9
Val Nimirenco <valnimirenco@gmail.com>
2021-02-07 14:19:40 -0800

    refactored variables

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-const express = require("express");
+var express = require("express");
 
-const PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 3000;
 
-const app = express();
+var app = express();
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
@@ -12,16 +12,16 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Set Handlebars.
-const exphbs = require("express-handlebars");
+var exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them.
-const routes = require("./controllers/burgerController.js");
+var routes = require("./controllers/burgerController.js");
 
 app.use(routes);
 
 app.listen(PORT, function () {
-    console.log("App now listening at localhost:" + PORT);
+  console.log("App now listening at localhost:" + PORT);
 });
