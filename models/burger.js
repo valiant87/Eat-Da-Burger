fc9d31db5880eee83bea13a6cd0a4da7603700ec
Val Nimirenco <valnimirenco@gmail.com>
2021-02-07 15:33:26 -0800

    updated functions that will interact with the database

--- models/burger.js
+++ models/burger.js
@@ -2,22 +2,22 @@
 var orm = require("../config/orm.js");
 
 var burger = {
-    all: function (cb) {
-        orm.all("burgers", function (res) {
-            cb(res);
-        });
-    },
-    // The variables cols and vals are arrays.
-    create: function (cols, vals, cb) {
-        orm.create("burgers", cols, vals, function (res) {
-            cb(res);
-        });
-    },
-    update: function (objColVals, condition, cb) {
-        orm.update("burgers", objColVals, condition, function (res) {
-            cb(res);
-        });
-    }
+  selectAll: function (cb) {
+    orm.selectAll("burgers", function (res) {
+      cb(res);
+    });
+  },
+  // The variables cols and vals are arrays.
+  insertOne: function (cols, vals, cb) {
+    orm.insertOne("burgers", cols, vals, function (res) {
+      cb(res);
+    });
+  },
+  insertOne: function (objColVals, condition, cb) {
+    orm.insertOne("burgers", objColVals, condition, function (res) {
+      cb(res);
+    });
+  },
 };
 
 // Export the database functions for the controller (burgerController.js).
