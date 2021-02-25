5d22d48345bdb8071c7e595ac0db01769bec13b1
Val Nimirenco <valnimirenco@gmail.com>
2021-02-24 19:54:02 -0800

    set enviroment/deployement

--- config/connection.js
+++ config/connection.js
@@ -10,7 +10,7 @@ if (process.env.JAWSDB_URL) {
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "password",
+    password: "process.env.DB_password",
     database: "burgers_db",
   });
 }
